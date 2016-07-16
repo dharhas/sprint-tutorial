14dff2fad06c282ead6d038e0ffaa03521d79074
Dharhas Pothina <dharhas@gmail.com>
2016-07-15 23:30:08 -0500

    add my_power to compute module to compute a^n

--- sprint_tutorial/compute.py
+++ sprint_tutorial/compute.py
@@ -6,3 +6,12 @@ def my_sum(x, y):
     """ Compute the sum of 2 numbers
     """
     return x + 2*y
+
+
+def my_power(a,n):
+    """ Compute a^n """
+    pow = a
+    for i in range(n-1):
+        pow = pow*a
+
+    return pow
