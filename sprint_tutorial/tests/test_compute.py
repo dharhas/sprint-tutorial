c38cb70a274d7b590d21ee67567c2c128108398a
Dharhas Pothina <dharhas@gmail.com>
2016-07-15 23:30:30 -0500

    add test for my_power

--- sprint_tutorial/tests/test_compute.py
+++ sprint_tutorial/tests/test_compute.py
@@ -1,9 +1,14 @@
 """ Tests for the compute module
 """
 
-from sprint_tutorial.compute import my_sum
+from sprint_tutorial.compute import my_sum, my_power
 import pytest
 
 def test_my_sum():
     assert my_sum(0, 0) == 0
     assert my_sum(3, 0) == 3
+
+
+def test_my_pow():
+    assert my_power(2, 4) == 16
+    assert my_power(5, 2) == 25
